765d44f294cb89d7bc41f9443c8f19271152b398
moumazumdar <himoumi@gmail.com>
2020-04-03 20:31:44 +0530

    Added ExceptionHandling and new fields in Patient Class

--- src/main/java/com/ibm/healthplanner/controller/HealthPlannerController.java
+++ src/main/java/com/ibm/healthplanner/controller/HealthPlannerController.java
@@ -2,8 +2,9 @@ package com.ibm.healthplanner.controller;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ibm.healthplanner.model.GetPatientResponse;
 import com.ibm.healthplanner.model.Patient;
 import com.ibm.healthplanner.service.HealthPlannerService;
+
 
 import io.swagger.annotations.Api;
 @RestController
@@ -28,6 +29,9 @@ import io.swagger.annotations.Api;
 @RequestMapping(value={"/","/healthplanner"})
 @Api(value="onlinestore", description="Operations pertaining to Health Advisor")
 public class HealthPlannerController {
+	
+	private static final Logger log = LoggerFactory.getLogger(HealthPlannerController.class);
+	
 	@Autowired
 	HealthPlannerService healthplannerService;
 	@GetMapping(value="/",produces = MediaType.APPLICATION_JSON_VALUE)
@@ -38,14 +42,13 @@ public class HealthPlannerController {
 	
     
 	 @PostMapping(value="/create/patient",headers="Accept=application/json")
-	 public ResponseEntity<Void> createUser(@RequestBody Patient patient, UriComponentsBuilder ucBuilder){
-	     System.out.println("Creating User "+patient.getName().toString());
-	     String id = "P" + "-" + System.currentTimeMillis();
-	     patient.setId(id);
+	 public ResponseEntity<?> createUser(@RequestBody Patient patient){
+	     
+		 log.debug("Creating User {}",patient.getName().getFirstName() + " " + patient.getName().getLastName());
+	    
 	     healthplannerService.createUser(patient);
-	     HttpHeaders headers = new HttpHeaders();
-	     headers.setLocation(ucBuilder.path("/patient/{id}").buildAndExpand(patient.getId()).toUri());
-	     return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+	     return new ResponseEntity<>("User " +patient.getName().getFirstName() + " " + patient.getName().getLastName()
+	    		 		+ " Created Successfully!!", HttpStatus.CREATED);
 	 }
 
 	 @GetMapping(value="/get/patient", headers="Accept=application/json")
@@ -65,7 +68,7 @@ public class HealthPlannerController {
 	  @PutMapping(value="/update/patient/{id}", headers="Accept=application/json") 
 	  public ResponseEntity<Patient> updateUser(@RequestBody Patient currentUser,@PathVariable("id") String id) {
 	  
-		  System.out.println("Current UserId is : "+currentUser.getId());
+		  log.info("Current UserId is : {} ",currentUser.getId());
 		  healthplannerService.update(currentUser, id);
 		  return new ResponseEntity<Patient>(HttpStatus.OK); 
 	  }
@@ -73,10 +76,7 @@ public class HealthPlannerController {
 	  @DeleteMapping(value="/delete/patient/{id}", headers ="Accept=application/json") 
 	  public ResponseEntity<Patient> deleteUser(@PathVariable("id") String id){
 		  
-		  Optional<Patient> user = healthplannerService.findPatientById(id);
-		  if (user == null) { 
-			  return new ResponseEntity<Patient>(HttpStatus.NOT_FOUND); 
-		  } 
+		  log.info(" UserId to delete is : {} ",id);		  
 		  healthplannerService.deleteUserById(id);
 		  return new ResponseEntity<Patient>(HttpStatus.NO_CONTENT); 
 	  }
--- src/main/java/com/ibm/healthplanner/model/Patient.java
+++ src/main/java/com/ibm/healthplanner/model/Patient.java
@@ -18,10 +18,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 @SuppressWarnings("serial")
 @Document(collection = "patients")
-//@JsonIgnoreProperties(value = { "dateOfBirth" })
 public class Patient  extends BaseEntity{
 	
-	//private String id;
 	@JsonProperty("patientName")
 	private PatientName name;
 	
@@ -30,9 +28,6 @@ public class Patient  extends BaseEntity{
 	
 	private String gender;
 	
-	//@JsonFormat(pattern="dd/mm/yyyy")
-	//@DateTimeFormat(iso=ISO.DATE_TIME)
-	//@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
 	private Date dateOfBirth;
 	
 	private String mailId;
@@ -44,12 +39,38 @@ public class Patient  extends BaseEntity{
 	
 	private List<String> medHistory;;
 	
+	private double height;
+	private double weight;
+	private double bmi;
+	
+	@JsonProperty("diet")
+	private String diet;
+	
+	@JsonProperty("activity")
+	private String activity; 
+	
+	@JsonProperty("tobacoUse")
+	private String tobacoUse;
+	
+	@JsonProperty("alchoholUse")
+	private String alchoholUse;
+	
+	@JsonProperty("caffineUse")
+	private String caffineUse;
+	
+	@JsonProperty("allergies")
+	private String allergies;
+	
+	@JsonProperty("allergicFrom")
+	private List<String> allergicFrom;
 	
 	public Patient(){
 		
 	}
 	public Patient(PatientName name, PostalAddress address, String gender,Date dateOfBirth, String mailId,
-			String phone, String maritalStatus, List<String> medHistory ){
+			String phone, String maritalStatus, List<String> medHistory, double height, double weight, 
+			double bmi, String diet, String activity, String tobacoUse, String alchoholUse, String caffineUse, 
+			String allergies, List<String> allergicfrom ){
 		
 		super();
 		this.name = name;
@@ -60,7 +81,18 @@ public class Patient  extends BaseEntity{
 		this.phone = phone;
 		this.maritalStatus = maritalStatus;
 		this.medHistory = medHistory;
+		this.height = height;
+		this.weight = weight;
+		this.bmi = bmi;
+		this.diet = diet;
+		this.activity = activity;
+		this.tobacoUse = tobacoUse;
+		this.alchoholUse = alchoholUse;
+		this.caffineUse = caffineUse;
+		this.allergies = allergies;
+		this.allergicFrom = allergicfrom;
 	}
+	
 	public PatientName getName() {
 		return name;
 	}
@@ -111,15 +143,75 @@ public class Patient  extends BaseEntity{
 		this.medHistory = medHistory;
 	}
 	
-	
-	private String toIndentedString(java.lang.Object o) {
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public double getBmi() {
+		return bmi;
+	}
+	public void setBmi(double weight, double height) {
+		this.bmi = this.getWeight()/(this.getHeight() * this.getHeight());
+	}
+		
+	public String getDiet() {
+		return diet;
+	}
+	public void setDiet(String diet) {
+		this.diet = diet;
+	}
+	public String getActivity() {
+		return activity;
+	}
+	public void setActivity(String activity) {
+		this.activity = activity;
+	}
+	public String getTobacoUse() {
+		return tobacoUse;
+	}
+	public void setTobacoUse(String tobacoUse) {
+		this.tobacoUse = tobacoUse;
+	}
+	public String getAlchoholUse() {
+		return alchoholUse;
+	}
+	public void setAlchoholUse(String alchoholUse) {
+		this.alchoholUse = alchoholUse;
+	}
+	public String getCaffineUse() {
+		return caffineUse;
+	}
+	public void setCaffineUse(String caffineUse) {
+		this.caffineUse = caffineUse;
+	}
+	public String getAllergies() {
+		return allergies;
+	}
+	public void setAllergies(String allergies) {
+		this.allergies = allergies;
+	}
+	public List<String> getAllergicFrom() {
+		return allergicFrom;
+	}
+	public void setAllergicFrom(List<String> allergicfrom) {
+		this.allergicFrom = allergicfrom;
+	}
+	/*private String toIndentedString(java.lang.Object o) {
 	    if (o == null) {
 	      return "null";
 	    }
 	    return o.toString().replace("\n", "\n    ");
-	  }
+	  }*/
 	
-	@Override
+	/*@Override
 	  public String toString() {
 	    StringBuilder sb = new StringBuilder();
 	    sb.append("class Patient {\n");
@@ -135,6 +227,6 @@ public class Patient  extends BaseEntity{
 	    
 	    sb.append("}");
 	    return sb.toString();
-	  }
+	  }*/
 	
 }
